3c48cbbc876679c4f59caffac18fdfa7eeb569ef
Justin Lee <38745924+plumsily@users.noreply.github.com>
2022-10-03 13:58:23 -0700

    updated map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -7,7 +7,7 @@ const Map = ({ trackListImgs, isSelected }) => {
     <CSSTransition in={isSelected} timeout={1500} classNames="map" appear>
       <div className="flex justify-self-end self-end row-start-3 row-end-4 col-start-4 col-end-5 z-20 h-max mb-6 mx-6 sm:w-[20rem] md:w-[30rem] xl:w-[40rem]">
         <img
-          className="object-contain saturate-[0.8] drop-shadow-[0_0_10px_rgba(251,146,60,0.4)]"
+          className="object-contain saturate-[0.8] drop-shadow-[0_0_10px_rgba(163,250,53,0.4)]"
           src={trackListImgs[0]?.map}
         ></img>
       </div>
